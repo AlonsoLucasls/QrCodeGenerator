e43289c05fb4f67ac6d4ab2f33857c15e2748448
AlonsoLucasls <lucassilvaalonso@gmail.com>
2020-07-16 10:17:59 -0300

    Initial Setup

---
+++ QrCodeGenerator/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using QrCodeGenerator.Models;
+using QRCoder;
+
+namespace QrCodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string nome,string link,string frase)
+        {
+
+            var data = new
+            {
+                nomeQr = nome != null ? nome : "Nome",
+                linkQr = link != null ? link : "www.qualuqerlink.com.br",
+                fraseQr = frase != null ? link : "Teste"
+            };
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(JsonConvert.SerializeObject(data), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(5);
+
+            var qrCodeRet = new byte[7000];
+
+            using (var memoryStream = new MemoryStream())
+            {
+                qrCodeImage.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                qrCodeRet = memoryStream.ToArray();
+            }
+
+            var result = new QrCodeViewModel { QrCodeData = qrCodeRet, Nome = nome,Link = link,Frase = frase };
+            var base64 = Convert.ToBase64String(result.QrCodeData);
+            var imgSrc = String.Format("data:image/gif;base64,{0}", base64);
+
+            ViewBag.Nome = nome != null ? nome : "Nome";
+            ViewBag.Link = link != null ? link : "www.qualuqerlink.com.br";
+            ViewBag.Frase = frase != null ? link : "Teste";
+            ViewBag.ImgQrCode = imgSrc;         
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QrCodeGenerator/Models/QrCodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrCodeGenerator.Models
+{
+    public class QrCodeViewModel
+    {
+        public string Nome { get; set; }
+        public string Link { get; set; }
+        public byte[] Img { get; set; }
+        public string Frase { get; set; }
+        public byte[] QrCodeData { get; set; }
+    }
+}
